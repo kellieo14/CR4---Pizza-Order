e1ef4f8064a19e6f03a20f248dd222495367a424
Kellie Olson <kellie.a.olson@biola.edu>
2016-04-08 14:59:24 -0700

    add js logic to place order

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 function Pizza(name, size, top, big, finTop) {
   this.customerName = name;
   this.pizzaSize= size;
@@ -6,28 +8,27 @@ function Pizza(name, size, top, big, finTop) {
   this.finTop = finTop;
 }
 
-Pizza.prototype.cost = function() {
-  var pizzaCost = this.pizzaSize + this.pizzaTop;
-  return pizzaCost;
+Pizza.prototype.Cost = function() {
+ var cost = this.pizzaSize + this.pizzaTop;
+ return cost;
 }
 
 $(document).ready(function() {
-
   $("#new-toppings").click(function(){
       $(".toppings").append('<h4>Select Your Topping</h4>'  +
                   '<select class="form-control new-top">' +
-                  '<option id="topping1" value="2">Pepperoni</option>' +
-                  '<option id="topping2" value="2">Chicken</option>' +
-                  '<option id="topping3" value="2">Bacon</option>' +
-                  '<option id="topping4" value="2">Sausage</option>' +
-                  '<option id="topping5" value="1">Sundried Tomatoes</option>' +
-                  '<option id="topping6" value="1">Peppers</option>' +
-                  '<option id="topping7" value="1">Cheese</option>' +
-                  '<option id="topping8" value="1">Olives</option>' +
-                  '<option id="topping9" value="1">Onions</option>' +
-                  '<option id="topping10" value="1">Pineapple</option>' +
+                  '<option id="topping1" value="2">  Pepperoni </option>' +
+                  '<option id="topping2" value="2">  Chicken </option>' +
+                  '<option id="topping3" value="2">  Bacon </option>' +
+                  '<option id="topping4" value="2">  Sausage </option>' +
+                  '<option id="topping5" value="1">  Sundried Tomatoes </option>' +
+                  '<option id="topping6" value="1">  Peppers </option>' +
+                  '<option id="topping7" value="1">  Cheese </option>' +
+                  '<option id="topping8" value="1">  Olives </option>' +
+                  '<option id="topping9" value="1">  Onions </option>' +
+                  '<option id="topping10" value="1"> Pineapple </option>' +
                   '</select>');
-      });
+    });
 
   $("form#new-order").submit(function(event) {
     event.preventDefault();
@@ -38,15 +39,13 @@ $(document).ready(function() {
     var inputtedSize = $(this).find("select.new-size option:selected").text();
     var inputtedTop = $(this).find("select.new-top option:selected").text();
     var newPizza = new Pizza(inputtedName, finalSize, finalTop, inputtedSize, inputtedTop);
-    console.log(newPizza.customerName);
-    console.log("$" + newPizza.cost());
+    console.log(newPizza.Cost());
 
-
-
-    $(".size").append(newPizza.pizzaSize);
+    $(".size").append(newPizza.pizzaBig);
     $(".name").append(newPizza.customerName);
     $("ul#toppings").append("<li>" + newPizza.finTop + "</li>");
-    $(".cost").append("$" + newPizza.cost());
+    $(".cost").append("$" + newPizza.Cost());
+
 
     });
   });
